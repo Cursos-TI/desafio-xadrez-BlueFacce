045e541cfa8922b7a0d9f99beb66ab4d9aebe869
Dev_Elias <eliasjonas033@gmail.com>
2025-03-03 23:10:57 +0000

    desafio novato

---
+++ xadrez_novato.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    //Movimento da torre
+    printf("Movimento da Torre:\n");
+    for(int i = 0; i < 5;i++){
+        printf("Direita\n");
+    }
+
+    //Movimento do bispo
+    printf("Movimento do Bispo:\n");
+    int casas_bispo = 5;
+    while (casas_bispo > 0){
+        printf("Cima Direita\n");
+        casas_bispo--;
+    }
+
+    //Movimento da rainha
+    printf("Movimento da Rainha:\n");
+    int casas_rainha = 8;
+    do{
+        printf("Esquerda\n");
+        casas_rainha--;
+    } while (casas_rainha > 0);
+
+    return 0;
+}
